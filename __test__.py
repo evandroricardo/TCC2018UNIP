985164b7bf096bab9570fb5236f6ffa380e9cdf9
Evandro2018 <escrittore@marketdata.com.br>
2018-09-02 16:18:18 -0300

    ajuste dos parametros

--- __test__.py
+++ __test__.py
@@ -5,11 +5,11 @@ from testes.teste_prefixado_semestral import TestePrefixadoSemestral
 from testes.teste_selic import TesteSelic
 
 testes = [
-    # {"test": TesteIPCA(), "params": (2207, 0.0494), "K": 2119.26},
-    # {"test": TesteIPCASemestral(), "params": (2937, 0.10), "K": 3286.48},
-    # {"test": TestePrefixado(), "params": (2346, 0.10, 0.15), "K": 0},
+    {"test": TesteIPCA(), "params": (2207, 0.0494), "K": 2119.26},
+    {"test": TesteIPCASemestral(), "params": (2937, 0.10), "K": 3286.48},
+    {"test": TestePrefixado(), "params": (1614, 0.10, 0.15), "K": 0},
     {"test": TestePrefixadoSemestral(), "params": (2610, 0.13), "K": 1025.55},
-    # {"test": TesteSelic(), "params": (1674, 0.15), "K": 8990},
+    {"test": TesteSelic(), "params": (1674, 0.15), "K": 8990},
 ]
 
 
